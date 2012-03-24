7d384a9e9a1fc9153ee70a18d435781ca4f5bcd9
karthikh <karthik.99h@gmail.com>
2012-03-24 01:25:52 -0700

    Removed public/index.html

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home Page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | Home")
     end
@@ -16,12 +16,12 @@ describe "StaticPages" do
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | Help")
     end
@@ -29,16 +29,30 @@ describe "StaticPages" do
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
 
   end
 
+  describe "Contact page" do
+    
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+ 
 end
